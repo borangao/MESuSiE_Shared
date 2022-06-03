f9733ccd8ea289685d939c73065775f3d3a9beda
borangao <borang@umich.edu>
2022-06-03 14:36:40 -0400

    fix bug of meSuSie plot

--- R/meSuSie_utility.R
+++ R/meSuSie_utility.R
@@ -195,9 +195,9 @@ meSusie_plot_pip<-function(res,R_mat,summary_data,annotation_data=NULL){
   
   names(summary_data)
   summary_data<-summary_stat_list
-  summary_data<-lapply(summary_data,function(x){
-    x$POS=seq(1,500,1)
-    return(x)})
+ # summary_data<-lapply(summary_data,function(x){
+ #   x$POS=seq(1,500,1)
+ #   return(x)})
   ##Create Mahattan Plot for GWAS Data
   
   for(x in 1:res$nancestry){
